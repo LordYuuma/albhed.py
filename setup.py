b76b431382f93c6987b7f02c922428b902595703
LordYuuma <lordyuuma@gmail.com>
2015-06-18 02:54:11 +0200

    update to use setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from distutils.core import setup
 
 setup(name='albhed.py',
       version='0.1',
-      description='',
+      description='An AlBhed Translator written in Python',
       author='Lord Yuuma von Combobreaker',
       author_email='lordyuuma@gmail.com',
       license="WTFPL",
+
       packages=['albhed'],
       py_modules=['albhed.maps', 'albhed.translators'],
       package_dir={'': 'src'},
+
+      scripts=['scripts/albhed']
       )
