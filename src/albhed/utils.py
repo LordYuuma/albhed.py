560e4b46690eb87ffe38e3a22774157da57c9ab7
LordYuuma <lordyuuma@gmail.com>
2015-06-28 07:27:50 +0200

    ArgumentHelper

--- src/albhed/utils.py
+++ src/albhed/utils.py
@@ -1,5 +1,33 @@
+from argparse import ArgumentParser, REMAINDER
 from collections import Iterable
 from re import escape, sub
+
+class ArgumentHelper(ArgumentParser):
+
+    def __init__(self, *args, **kwargs):
+        ArgumentParser.__init__(self, *args, **kwargs)
+        self.add_argument("-a", "--al-bhed", nargs=2, type=str, metavar=("start", "end"),
+                          help="translate to Al Bhed", default=("\"[", "\"]"))
+        self.add_argument("-d", "--delimiters", nargs=2, type=str, metavar=("start", "end"),
+                          help="set delimiters for proper nouns", default=("\"[", "\"]"))
+        self.add_argument("-n", "--nouns", help="detect proper nouns from file")
+        self.add_argument("-r", "--remove", type=str, metavar="rm",
+                          help="characters to delete in output", default="[]")
+        self.add_argument("-s", "--spiran", action="store_true",
+                          help="translate to spiran")
+        self.add_argument("-t", "--traditional", "--only-canon", action="store_true",
+                          help="disable translation of non-canon symbols")
+
+        self.add_argument("file", nargs=REMAINDER)
+
+    @staticmethod
+    def getTranslateWay(translator, args, default="toSpiran"):
+        if self.al_bhed and not self.spiran:
+            return translator.toAlBhed
+        elif self.spiran and not self.al_bhed:
+            return translator.toSpiran
+        else:
+            return getattr(translator, default)
 
 class ProperNounDetector(object):
 
